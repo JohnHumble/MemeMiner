611e397bc73e27c81c15ff3e79e1bb7e158f92ea
John Humble <johnhhumble@gmail.com>
2019-03-30 10:48:52 -0600

    now we have a button that does things

--- app/src/main/java/com/example/mememiner/MainActivity.java
+++ app/src/main/java/com/example/mememiner/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.mememiner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Member;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button baitButton;
+    TextView words;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        baitButton = findViewById(R.id.bait_button);
+        words = findViewById(R.id.words);
+
+        baitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                words.setText("Tee Hee :)");
+            }
+        });
     }
 }
